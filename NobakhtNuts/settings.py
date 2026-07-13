e01a6ee8c01338b34721f86cc5526813a51f5a3c
Amir Nobakht <amirmhmdnobakht1387@gmail.com>
2026-07-13 17:34:30 +0330

    Update settings.py

--- NobakhtNuts/settings.py
+++ NobakhtNuts/settings.py
@@ -89,8 +89,9 @@ else:
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT'),
+            "OPTIONS": {
+                "unix_socket": "/var/lib/mysql/mysql.sock",
+            },
         }
     }
 
