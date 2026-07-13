f67a8c062b7c04b2b5bd322b97e402a92b8a4309
Amir Nobakht <amirmhmdnobakht1387@gmail.com>
2026-07-13 20:26:19 +0330

    Update urls.py

--- NobakhtNuts/urls.py
+++ NobakhtNuts/urls.py
@@ -2,24 +2,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include, re_path
 from django.views.static import serve
 
 urlpatterns = [
-
-
-    path("", lambda request: HttpResponse("DJANGO OK")),
-    # path('admin/', admin.site.urls),
-    # path('adminpanel/' ,include('adminpanel_module.url')),
-    # path('' , include('home_module.url')),
-    # path('accounts/' ,include('account_module.url')),
-    # path('userpanel/' ,include('userpanel_module.url')),
-    # path('support/' ,include('support_module.url')),
-    # path('products/' ,include('product_module.url')),
-    # path('orders/' ,include('order_module.url')),
-    # path('docs/' ,include('documents_module.url')),
-    # path('articles/' ,include('article_module.url')),
+    path('admin/', admin.site.urls),
+    path('adminpanel/' ,include('adminpanel_module.url')),
+    path('' , include('home_module.url')),
+    path('accounts/' ,include('account_module.url')),
+    path('userpanel/' ,include('userpanel_module.url')),
+    path('support/' ,include('support_module.url')),
+    path('products/' ,include('product_module.url')),
+    path('orders/' ,include('order_module.url')),
+    path('docs/' ,include('documents_module.url')),
+    path('articles/' ,include('article_module.url')),
 ]
 
 urlpatterns += [
