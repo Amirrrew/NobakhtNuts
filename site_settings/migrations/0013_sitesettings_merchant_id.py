c731e9f1f56a9169507d69f2beb489666d102144
Amir Nobakht <amirmhmdnobakht1387@gmail.com>
2026-07-13 19:16:23 +0330

    Update 0013_sitesettings_merchant_id.py

--- site_settings/migrations/0013_sitesettings_merchant_id.py
+++ site_settings/migrations/0013_sitesettings_merchant_id.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='sitesettings',
             name='merchant_id',
-            field=models.CharField(blank=True, null=True, verbose_name='مرچنت آیدی درگاه'),
+            field=models.CharField(max_length=1000 ,blank=True, null=True, verbose_name='مرچنت آیدی درگاه'),
         ),
     ]
