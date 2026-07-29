564a313b33224bf04f9f77d5acfe4cccac774098
Amir Nobakht <amirmhmdnobakht1387@gmail.com>
2026-07-29 17:16:19 +0330

    collect new static

---
+++ staticfiles/scripts/service-worker.js
@@ -0,0 +1,45 @@
+// static/sw.js
+const CACHE_NAME = 'nobakhtnuts-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/main.css',
+    '/static/js/main.js',
+    '/static/icons/icon-192x192.png',
+    '/offline/',
+];
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/offline/');
+                }
+            });
+        })
+    );
+});
