5fb8eaff6320e6ae45fbae6946cc624f4e0e870a
Amir Nobakht <amirmhmdnobakht1387@gmail.com>
2026-07-30 17:34:58 +0330

    delete static

--- staticfiles/scripts/service-worker-starter.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(() => console.log('service worker registered'))
-        .catch((err) => console.log('service worker registration failed' ,err))
-}
